91142cd682a0afb614f6b69876e3350d80bbe1a5
Adriano Margarin <adriano.margarin@gmail.com>
2018-12-01 10:36:43 -0200

    Validação da inscriçãe

--- redeneural/subscriptions/forms.py
+++ redeneural/subscriptions/forms.py
@@ -8,3 +8,18 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = models.Subscription
         fields = ['name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        self.event = kwargs.pop('event', None)
+
+        super(SubscriptionForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        data = self.cleaned_data.copy()
+
+        email = data.get('email')
+
+        if email and self.event.subscriptions.filter(email=email).exists():
+            raise forms.ValidationError('Sua inscrição já está feita para esse evento.')
+
+        return data
